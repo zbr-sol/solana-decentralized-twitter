e7069dbee4d103432723190fced325041fbb9906
osuburger <zboerger@gmail.com>
2022-02-04 10:21:34 -0500

    Add a simple 'can send a new tweet' test to our anchor test suite

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -1,6 +1,7 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { SolanaTwitter } from '../target/types/solana_twitter';
+import * as assert from "assert";
 
 describe('solana-twitter', () => {
 
@@ -9,16 +10,19 @@ describe('solana-twitter', () => {
 
   const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    await program.rpc.sendTweet('TOPIC HERE', 'CONTENT HERE', {
-      accounts: {
-          // Accounts here...
-      },
-      signers: [
-          // Key pairs of signers here...
-      ],
-  });
-    
+  it('can send a new tweet', async () => {
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet('NBA', 'How about these all star selections??', {
+        accounts: {
+            tweet: tweet.publicKey,
+            author: program.provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [tweet],
+    });
+
+    // Fetch the account details of the created tweet.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+  	console.log(tweetAccount);
   });
 });
