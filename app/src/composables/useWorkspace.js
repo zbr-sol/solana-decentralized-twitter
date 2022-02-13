a836dc186848df95ee69bacaa03cc0cac88ac004
osuburger <zboerger@gmail.com>
2022-02-13 10:41:28 -0500

    Add some .env config files and update useWorkspace to use the appropriate environment variable when setting the target clusterURL

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -8,12 +8,13 @@ const programID = new PublicKey(idl.metadata.address)
 let workspace = null
 const preflightCommitment = 'processed'
 const commitment = 'processed'
+const clusterUrl = process.env.VUE_APP_CLUSTER_URL
 
 export const useWorkspace = () => workspace
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
-    const connection = new Connection('https://api.devnet.solana.com', commitment)
+    const connection = new Connection(clusterUrl, commitment)
     const provider = computed(() => new Provider(connection, wallet.value, { preflightCommitment, commitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
 
